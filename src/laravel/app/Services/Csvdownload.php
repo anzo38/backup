f7d9637610acdb850014c323bd82cb133bdc1cf3
yamazaki <yamazaki@uns-j.co.jp>
2021-05-14 16:56:09 +0900

    dockerにHTTPS接続できるようsslを設置

--- src/laravel/app/Services/Csvdownload.php
+++ src/laravel/app/Services/Csvdownload.php
@@ -23,88 +23,46 @@ class  CsvDownload
     public function download($contact_id)
     {
 
-        $csv_data="id,氏名,好きな食べ物, お住まいの地域, ログインID,パスワード"."\r\n";
+        $csv_data="id,氏名,趣味,好きな食べ物,お住まいの地域,ログインID,パスワード"."\r\n";
         // $contact_id=$id;
         // $item = Contact::find($contact_id);
-        $contacts=Contact::select('id')->with('hobby')->get()->toarray(); 
+        // $contacts=Contact::select('id')->with('hobby')->get()->toarray(); 
         $d_q='"';
         $empty="空";
+        
         // $contacts=Contact::select('id')->with('hobby')->get()->toarray(); 
-        // $contacts=Contact::with('hobbys:hobby,contact_id')->get()->toarray();
+        $contacts=Contact::with('hobbys:hobby,contact_id')->get()->toarray();
         
-        
-    //    dd($item);exit;
-    
-    //    dd($contacts);exit;
-    // foreach ($contacts as $r =>$e){
-    //     foreach($e as $f=>$m){
-    //         $m['hobbys'];
-    //         dd( $m['hobbys']);exit;
-    //         if( $e['hobbys'][0]['contact_id'] == $e['id']){
-              
-    //                return $csv_data.= $d_q .  $e['hobbys'][0]['hobby'] . $d_q .",";
-    //             }
-    //     }
-      
-    //       }
-    // dd($csv_data);exit;
-    // echo $b;exit;
-    //    dd($m);exit;
+//    dd($contacts);exit;
         foreach ($contacts as $k => $v) {
       
             $csv_data.= $d_q .  $v['id'] . $d_q .",";
             $csv_data.= $d_q .  $v['name'] . $d_q .",";
             
-           foreach( $v['hobbys'] as $o => $c){
-          
-               $csv_data.= $d_q .  $c['hobby'] . $d_q .","."\r\n";
-               if(empty($c['hobby'])){
-                $csv_data.= $d_q .   $empty . $d_q .",";
-                break;
-               }
-            //    break;
-           }
-          
+            dd($v['hobbys']);exit;
               
-            
+            foreach( $v['hobbys'] as $o => $c){
+              if($c==[]){
+                    $c[]=$c['hobby']['hobby'];
+                    $csv_data.= $d_q .  $c . $d_q .",";
+                }
+             
+                $csv_data.= $d_q .  $c['hobby']['hobby'] . $d_q .",";
+                dd($c['hobby']);
+                }
+             
             $csv_data.= $d_q .  $v['food'] . $d_q .",";
             $csv_data.= $d_q .  $v['area'] . $d_q .",";
             $csv_data.= $d_q .  $v['login'] . $d_q .",";
+
+        
             $csv_data.= $d_q .  $v['password'] . $d_q .","."\r\n";
             // $csv_data.= $d_q .  $v['hobbys']['hobby'] . $d_q .",
             
         
-                // foreach($contact as $e =>$d){
-                //     if($f=="hobby"){
-                //         $csv_data.= $d_q . $contact . $d_q .","."\r\n";
-                //     }
-                
-
-                // }
-      
-            
-           
-            // $csv_data.= $d_q . $contact->name . $d_q .",";
-           
-          
-            // $csv_data.= $d_q . $contact->contact_id . $d_q .",";
-            // $csv_data.= $d_q . $contact->food . $d_q .",";
-            // $csv_data.= $d_q . $contact->area . $d_q .",";
-            // $csv_data .= $d_q . $contact->login . $d_q  .",";
-            // $csv_data .= $d_q . $contact->password . $d_q  .","."\r\n";
         
         }
-        // $contacts=Hobby::all();
-        // foreach($contacts as $contact){
-        //     $csv_data .= $d_q . $contact->password . $d_q  .","."\r\n";
-        // }
-     
-    //    dd ($csv_data);exit;
       
-
-    // return $csv_data;
-
-        // }
 
             //CSVダウンロード
             $csv_name = date('Ymd-His') . '.csv';
--- src/laravel/resources/views/admin/member.blade.php
+++ src/laravel/resources/views/admin/member.blade.php
@@ -1,4 +1,4 @@
- {{-- @foreach($contact_id as $k =>$v) --}}
+ @foreach($contact_id as $k =>$v)
 {{-- {{$v['id']."/r\n"}} --}}
 <a href="{{ action('AdminController@downloads', $v['id']) }}">{{ 'download'.$v['id'] }}</a><br>
 {{-- {{Form::open(['url' => route('.index', ['id' => '1']), 'method' => 'get'])}} --}}
