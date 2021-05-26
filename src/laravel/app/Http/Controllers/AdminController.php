082001fda4eb5444015be241224cc9af555f92f4
anzo38 <yamazaki@uns-j.co.jp>
2021-05-26 11:01:24 +0900

    CSV出力の不明な文字の削除

--- src/laravel/app/Http/Controllers/AdminController.php
+++ src/laravel/app/Http/Controllers/AdminController.php
@@ -2,44 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Model\Contact;
-use Illuminate\Support\Facades\Auth;
-use App\Providers\DownloadServiceProvider;
 
+
+use App\Services\Csvdownload;
 
 class AdminController extends Controller
 {
+    
     public function __construct() {
         $this->middleware('auth:web');
         // $this->middleware('guest')->except('logout');
+
     }
 
     
 
-    public function member(\App\Services\Csvdownload $csvdownload) {
+    // public function member(\App\Services\Csvdownload $csvdownload) {
+    //     $contact_id =$csvdownload->getContactTableId();
+       
+    //     return view('admin.member',['contact_id'=> $contact_id]);
+    // }
+    // メソッドインジェクション?
+    public function member(Csvdownload $csvdownload) {
         $contact_id =$csvdownload->getContactTableId();
        
         return view('admin.member',['contact_id'=> $contact_id]);
     }
     
 
-    public function downloads($id, \App\Services\Csvdownload $csvdownload){
+    public function downloads($id,Csvdownload $csvdownload){
         // dd($id);exit;
         $conrtact_id = $id;
         $data = $csvdownload->download($conrtact_id);
-        dd($id);
+        // dd($id);
         
         // return view('admin.downloads',['data'=> $data,]);
 
     }
 
-    // public function download(Request $request) {
-      
-    //     app()->bind(\App\CsvDownload::class, function() {				
-    //         return new \App\CsvDownload();				
-    //     });	
 
-    // }
         
 }
